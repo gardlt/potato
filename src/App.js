9f5b64b7e52d460d5e1c1048ab5d0c4a27d6da3f
gardlt <gardlt@tetrasol.org>
2018-03-28 21:35:49 -0500

    update-cname

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import fontawesome from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import brands from '@fortawesome/fontawesome-free-brands'
+
+
+
 import logo from './no_computers_wrtr0a.gif';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -14,12 +21,19 @@ class App extends Component {
         <p className="App-intro">
             I am a software developer. I convert coffee into code and fun stuff.
             I mostly deal with Python and Javascript for my coding. I also have
-            fun with docker containers.
+            fun with docker containers. <code>docker run </code>
         </p>
         <p>
-            <a href="https://github.com/gardlt">Github</a>
-            <a href="https://twitter.com/gardlt">Twitter</a>
-            <a href="www.linkedin.com/in/alexis-rivera-de-la-torre-4b265653">LinkedIn</a>
+            <FontAwesomeIcon icon="check-square"/>
+            <a href="https://github.com/gardlt">
+                <FontAwesomeIcon icon={["fab", "github"]} />
+            </a>
+            <a href="https://twitter.com/gardlt">
+                <FontAwesomeIcon icon={["fab", "twitter"]} />
+            </a>
+            <a href="www.linkedin.com/in/alexis-rivera-de-la-torre-4b265653">
+                <FontAwesomeIcon icon={["fab", "linkedin"]} />
+            </a>
         </p>
       </div>
     );
